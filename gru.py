8c4f2310bd39d70b6b075cf39b1c528fe6adc678
andrada <andrapumnea@gmail.com>
2018-08-11 16:41:47 +0200

    fix bug in gru

--- gru.py
+++ gru.py
@@ -41,10 +41,10 @@ def create_model(word_embedding_matrix, maxlen=30, lr=1e-3):
     distance = Lambda(preprocessing.exponent_neg_manhattan_distance, output_shape=preprocessing.get_shape)(
         [output_q1, output_q2])
 
-    output = concatenate([encoded_q1, encoded_q2, distance])
+    output = concatenate([encoded_q1,output_q1, output_q2])
     output = Dense(1, activation='relu')(output)
     output = BatchNormalization()(output)
-    
+
     output = Dense(1, activation='sigmoid')(output)
 
     # Pack it all up into a model
